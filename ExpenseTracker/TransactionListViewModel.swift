53b4fead40bdf31852883a2eea3777f8dbbb622e
Mackley <mackleymagalhaess@hotmail.com>
2023-12-20 17:15:38 -0300

    Data Task Publisher

---
+++ ExpenseTracker/TransactionListViewModel.swift
@@ -0,0 +1,47 @@
+//
+//  TransactionListViewModel.swift
+//  ExpenseTracker
+//
+//  Created by Mackley Magalhães da Silva on 20/12/23.
+//
+
+import Foundation
+import Combine
+
+final class TransactionListViewModel: ObservableObject {
+    @Published var transactions: [Transaction] = []
+    
+    private var cancellables = Set<AnyCancellable>()
+    
+    init() {
+        getTransactions()
+    }
+    
+    func getTransactions() {
+        guard let url = URL(string: "https://designcode.io/data/transactions.json") else {
+            print("Invalid URL")
+            return
+        }
+        
+        URLSession.shared.dataTaskPublisher(for: url)
+            .tryMap { (data, response) -> Data in
+                guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+                    throw URLError(.badServerResponse)
+                }
+                return data
+            }
+            .decode(type: [Transaction].self, decoder: JSONDecoder())
+            .receive(on: DispatchQueue.main)
+            .sink { completion in
+                switch completion {
+                case .failure(let error):
+                    print("Error fetching transactions", error.localizedDescription)
+                case .finished:
+                    print("Finished fetching transactions")
+                }
+            } receiveValue: { [weak self] result in
+                self?.transactions = result
+            }
+            .store(in: &cancellables)
+    }
+}
